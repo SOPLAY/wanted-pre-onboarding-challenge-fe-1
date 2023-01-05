2402ac21b200079e096f34205f642e934f1ada68
SOPLAY <tkdgns1341@gmail.com>
2023-01-05 18:34:24 +0900

    Feat: apis Todo 로직 추가
    apis.Todo 로직 추가
     - apis.getTodos()
     - apis.getTOdosById()
     - apis.createTodo()
     - apis.updateTodo()
     - apis.deleteTodo()

--- src/apis/apis.ts
+++ src/apis/apis.ts
@@ -4,22 +4,23 @@ const apiServerUrl = "http://localhost:8080";
 
 export type IAuthLogin = { email: string; password: string };
 export type IAuthSignUp = IAuthLogin;
-
 type IResAuth = { message: string; token: string };
+export type IGetTodosById = { id: string };
+export type ITodo = { title: string; content: string };
 
 //api auth
-const signIn = (data: IAuthLogin): Promise<AxiosResponse<IResAuth>> =>
-  axios.post(`${apiServerUrl}/users/login`, data);
+const signIn = (body: IAuthLogin): Promise<AxiosResponse<IResAuth>> =>
+  axios.post(`${apiServerUrl}/users/login`, body);
+const signUp = (body: IAuthSignUp): Promise<AxiosResponse<IResAuth>> =>
+  axios.post(`${apiServerUrl}/users/create`, body);
 
-const signUp = (data: IAuthSignUp): Promise<AxiosResponse<IResAuth>> =>
-  axios.post(`${apiServerUrl}/users/create`, data);
 // api todos
-
-const getTodos = () => {};
-const getTodosById = () => {};
-const createTodo = () => {};
-const updateTodo = () => {};
-const deleteTodo = () => {};
+const getTodos = () => axios.get(`${apiServerUrl}/todos`);
+const getTodosById = (id: string) => axios.get(`${apiServerUrl}/todos/${id}`);
+const createTodo = (body: ITodo) => axios.post(`${apiServerUrl}/todos`, body);
+const updateTodo = (id: string, body: ITodo) =>
+  axios.put(`${apiServerUrl}/todos/${id}`, body);
+const deleteTodo = (id: string) => axios.delete(`${apiServerUrl}/todos/${id}`);
 
 const apis = {
   auth: {
