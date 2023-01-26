1f71f27625b8a6321f9a23c3ca798f1ac054cf95
SOPLAY <tkdgns1341@gmail.com>
2023-01-26 23:46:14 +0900

    Fix: apis return type 지정

--- src/apis/apis.ts
+++ src/apis/apis.ts
@@ -1,27 +1,39 @@
-import axios, { AxiosResponse } from "axios";
+import { AxiosResponse } from "axios";
+import axios from "./axios";
+import { ITodoItem } from "@src/types/todo";
+import { IAuthSignIn, IAuthSignUp, IResAuth } from "@src/types/auth";
 
 const apiServerUrl = "http://localhost:8080";
 
-export type IAuthLogin = { email: string; password: string };
-export type IAuthSignUp = IAuthLogin;
-type IResAuth = { message: string; token: string };
-export type IGetTodosById = { id: string };
+// export type IGetTodosById = { id: string };
 export type ITodo = { title: string; content: string };
 
 //api auth
-const signIn = (body: IAuthLogin): Promise<AxiosResponse<IResAuth>> =>
+const signIn = (body: IAuthSignIn): Promise<AxiosResponse<IResAuth>> =>
   axios.post(`${apiServerUrl}/users/login`, body);
 const signUp = (body: IAuthSignUp): Promise<AxiosResponse<IResAuth>> =>
   axios.post(`${apiServerUrl}/users/create`, body);
 
 // api todos
-const getTodos = () =>
-  axios.get(`${apiServerUrl}/todos`).then((res) => res.data.data);
-const getTodosById = (id: string) => axios.get(`${apiServerUrl}/todos/${id}`);
-const createTodo = (body: ITodo) => axios.post(`${apiServerUrl}/todos`, body);
-const updateTodo = (id: string, body: ITodo) =>
+const getTodos = (): Promise<AxiosResponse<{ data: ITodoItem[] }>> =>
+  axios.get(`${apiServerUrl}/todos`);
+
+const getTodosById = (
+  id: string
+): Promise<AxiosResponse<{ data: ITodoItem }>> =>
+  axios.get(`${apiServerUrl}/todos/${id}`);
+
+const createTodo = (body: ITodo): Promise<AxiosResponse<ITodoItem>> =>
+  axios.post(`${apiServerUrl}/todos`, body);
+
+const updateTodo = (
+  id: string,
+  body: ITodo
+): Promise<AxiosResponse<ITodoItem>> =>
   axios.put(`${apiServerUrl}/todos/${id}`, body);
-const deleteTodo = (id: string) => axios.delete(`${apiServerUrl}/todos/${id}`);
+
+const deleteTodo = (id: string): Promise<AxiosResponse<{ data: null }>> =>
+  axios.delete(`${apiServerUrl}/todos/${id}`);
 
 const apis = {
   auth: {
